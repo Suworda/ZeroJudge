f9e1f9c655bfcdc1f05cd1220574f6c72ad29100
Suworda <su5200su@gmail.com>
2026-03-20 21:44:37 +0800

    Update default code.cpp

--- default code.cpp
+++ default code.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define ll long long
 
-int main(){
+signed main(){
     // ios::sync_with_stdio(false);
     // cin.tie(0);
-    
-    
 
 }
